b511b337a0ce657d7e3247e994197ca46ae4280f
nhatbao1504 <hnhatbao1504@gmail.com>
2024-08-23 14:27:11 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var number = 4;
+function add(x, y) {
+    return x + y;
+}
+console.log(add(number, 10));
---
+++ index.ts
@@ -0,0 +1,7 @@
+const number = 4;
+
+function add(x: number, y: number) {
+    return x + y;
+}
+
+console.log(add(number, 10))
